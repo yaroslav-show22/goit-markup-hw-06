5d472fc5a899aeb47a73f0e972c74b987128aea3
Kastiel <yaros2205@gmail.com>
2025-04-15 14:58:00 +0200

    my brain is overheated

--- js/modal.js
+++ js/modal.js
@@ -13,6 +13,6 @@
 
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle("is-open");
+    refs.modal.classList.toggle("is-open-modal");
   }
 })();
